d7203fa91164725c4b96aee887c604655cba6f60
wenwl <Wwllong97@outlook.com>
2020-06-29 20:58:55 +0800

    feat:状态模式

---
+++ base/c3_mediator/mediator_frame.py
@@ -0,0 +1,27 @@
+# 中介模式-代码框架
+
+class InteractiveObject:
+    """进行交互的对象"""
+    pass
+
+class InteractiveObjectImplA:
+    """实现类A"""
+    pass
+
+class InteractiveObjectImplB:
+    """实现类B"""
+    pass
+
+class Meditor:
+    """中介类"""
+
+    def __init__(self):
+        self.__interactiveObjA = InteractiveObjectImplA()
+        self.__interactiveObjB = InteractiveObjectImplB()
+
+    def interative(self):
+        """进行交互的操作"""
+        # 通过self.__interactiveObjA和self.__interactiveObjB完成相应的交互操作
+        pass
+
+
